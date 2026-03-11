3e9a669b30098f58b76865d7b996f2c4a19bd261
carlos <carlos@carloss-MacBook-Pro.local>
2026-03-11 13:04:46 +0000

    adding first folders

---
+++ helloWolrd.java
@@ -0,0 +1,21 @@
+
+public class helloWolrd {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    
+    }
+}
+
+
+// create a method that sums two numbers and returns the result
+public class helloWolrd {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int sum = sum(5, 10);
+        System.out.println("The sum of 5 and 10 is: " + sum);
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
